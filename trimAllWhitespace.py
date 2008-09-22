6b93c0045f1cf7ead20aac82f7dbd85f4f4a6c38
Dietmar Winkler <Dietmar.Winkler@TU-Berlin.de>
2008-09-22 18:00:03 +0200

    try to make the path exclusion more "pythonish"

--- trimAllWhitespace.py
+++ trimAllWhitespace.py
@@ -16,7 +16,7 @@ import os
 def main(args):
     for path, dirs, files in os.walk(args[1]):
         for file in files:
-            if path.find(".svn") == -1 and path.find(".git") == -1:
+            if not ".svn" in path and not ".git" in path:
                 trimWhitespace(addTrailingSlash(path) + file)
             else:
                 print "excluding " + addTrailingSlash(path) + file # DEBUG
