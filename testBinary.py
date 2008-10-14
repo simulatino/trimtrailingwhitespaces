d5f9f031e25c4873f519e51e544ca816a96d4870
Dietmar Winkler <Dietmar.Winkler@TU-Berlin.de>
2008-10-14 15:21:58 +0200

    Added recursion to the testBinary function.
     * now the testBinary will walk through the given list of directories
       (current directory by default) and list all type of file

--- testBinary.py
+++ testBinary.py
@@ -5,6 +5,7 @@ import string, sys
 text_characters = "".join(map(chr, range(32, 127)) + list("\n\r\t\b"))
 _null_trans = string.maketrans("", "")
 
+# test for text file
 def istextfile(filename, blocksize = 512):
     return istext(open(filename).read(blocksize))
 
@@ -26,32 +27,51 @@ def istext(s):
         return 0
     return 1
 
+# help message on usage
+def usage(argv):
+    print "\n Usage: %s  <directory> [<directory> ...]" % (argv[0],)
+    print """
+  Shows recursively which files in a directory are text
+  and which are binary.
+
+  If no directory is given, the current directory '.' 
+  is used as default.
+  """
+ 
+# warning message for unkown options
+def unkownOption(argv):
+    print "Unknown option !" 
+    print " Use %s -h [--help] for usage instructions." % (argv[0],)
+
 def main(argv):
     import os, getopt
+    # look for optional arguments
     try:
-        args, dirnames = getopt.getopt(argv[1:], "h", ["help"])
-    except getopt.error:
-        args = "dummy"
-    if args:
-        print "Usage: %s <directory> [<directory> ...]" % (argv[0],)
-        print " Shows which files in a directory are text and which are binary"
+        opts, dirnames = getopt.getopt(argv[1:], "h", ["help"])
+    # unkown option is given trigger the display message
+    except getopt.GetoptError:
+        unkownOption(argv)
         sys.exit(0)
-
     table = {0: "binary", 1: "text"}
+    for o, a in opts:
+        if o == "-h" or "--help":
+            usage(argv)
+            sys.exit(0)
+        else:
+            unkownOption(argv)
+            sys.exit(0)
+    # default directory
     if not dirnames:
         dirnames = ["."]
+    # walk recursively through the given directories
     for dirname in dirnames:
-        try:
-            filenames = os.listdir(dirname)
-        except OSError, err:
-            print >>sys.stderr, err
-            continue
-        for filename in filenames:
-            fullname = os.path.join(dirname, filename)
-            try:
-                print table[istextfile(fullname)], repr(fullname)[1:-1]
-            except IOError:  # eg, this is a directory
-                pass
+        for path, dirs, files in os.walk(dirname):
+            for file in files:
+                fullname = os.path.join(path, file)
+                try:
+                    print table[istextfile(fullname)], repr(fullname)[1:-1]
+                except IOError:  # eg, this is a directory
+                    pass    
     
 if __name__ == "__main__":
     main(sys.argv)
