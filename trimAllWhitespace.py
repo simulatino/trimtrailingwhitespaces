be480f775db7bc8eb2fd7755706776aca2e91828
Dietmar Winkler <Dietmar.Winkler@TU-Berlin.de>
2008-10-17 17:16:08 +0200

    replace FND's custom path correction function with the built-in os.path.join function

--- trimAllWhitespace.py
+++ trimAllWhitespace.py
@@ -7,16 +7,16 @@ excluded.
 Usage: 	python trimAllWhitespace.py <DestinationDir>
 """
 
-import sys
-import os
+import os, sys
 
 def main(args):
 	for path, dirs, files in os.walk(args[1]):
 		for file in files:
 			if not ".svn" in path and not ".git" in path:
-				trimWhitespace(addTrailingSlash(path) + file)
+				fullname = os.path.join(path, file)
+				trimWhitespace(fullname)
 			else:
-				print "excluding " + addTrailingSlash(path) + file # DEBUG
+				print "excluding " + fullname # DEBUG
 
 def trimWhitespace(filepath):
 	lines = []
@@ -24,11 +24,6 @@ def trimWhitespace(filepath):
 		lines.append(line.rstrip())
 	f = open(filepath, "w")
 	f.write("\n".join(lines) + "\n")
-
-def addTrailingSlash(path):
-	if path[-1] != os.sep:
-		path = path + os.sep
-	return path
 
 if __name__ == "__main__":
 	sys.exit(main(sys.argv))
