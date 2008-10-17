3fb8a4c3845254c35d01a2c914e09b668fd71603
Dietmar Winkler <Dietmar.Winkler@TU-Berlin.de>
2008-10-17 18:28:26 +0200

    Added the binary checker based on libmagic to only trim text files.
     * as of now this works perfectly on LinuX machines if magic.py is present in the same directory

--- trimAllWhitespace.py
+++ trimAllWhitespace.py
@@ -7,23 +7,37 @@ excluded.
 Usage: 	python trimAllWhitespace.py <DestinationDir>
 """
 
-import os, sys
+import os, sys, magic, textwrap
 
+mime = magic.Magic(mime=True)
+
+# detect the mime type of the text file
+def detecttype(filepath):
+	type = mime.from_file(filepath)
+	if "text/" in type:
+		return "text"
+	else:
+		return type
+# walk through the given path and call trim function for text files
 def main(args):
 	for path, dirs, files in os.walk(args[1]):
 		for file in files:
-			if not ".svn" in path and not ".git" in path:
-				fullname = os.path.join(path, file)
-				trimWhitespace(fullname)
+			filepath = os.path.join(path, file)
+			filetype=detecttype(filepath)
+			if not ".svn" in path and not ".git" in path and filetype=="text":
+				print "trimming " + filepath
+				trimWhitespace(filepath)
 			else:
-				print "excluding " + fullname # DEBUG
+				print "skipping binary file of type " +filetype + ": "+ filepath
 
+# trim trailing white spaces from a given filepath
 def trimWhitespace(filepath):
 	lines = []
 	for line in open(filepath, "r"):
 		lines.append(line.rstrip())
 	f = open(filepath, "w")
 	f.write("\n".join(lines) + "\n")
+	f.close
 
 if __name__ == "__main__":
 	sys.exit(main(sys.argv))
