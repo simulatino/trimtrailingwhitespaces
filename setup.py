06cebfde30a313b9b384e7d0e70139a85d056f81
Dietmar Winkler <Dietmar.Winkler@hit.no>
2010-04-20 11:32:39 +0200

    Added more description to setup.py. Version number up to 0.2 now.

--- setup.py
+++ setup.py
@@ -12,4 +12,14 @@ Prerequisites: py2exe from www.py2exe.org
 from distutils.core import setup
 import py2exe
 
-setup(console=['ttws.py'])
+setup(
+    console=['ttws.py'],
+    name = 'ttws',
+    version = 0.2,
+    description = 'Script to remove trailing whitespaces from textfiles.',
+    author = 'Dietmar Winkler',
+    author_email = 'http://claimid/dietmarw',
+    platforms = 'Posix; MacOS X; Windows',
+    packages = find_packages(exclude=['test']),
+    include_package_data = True
+    )
