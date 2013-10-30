8822ea32175aae69f4f53897d2c378cdc9dd63c1
FND <fnd@localhost.localdomain>
2013-10-30 16:46:16 +0100

    moved command-line interface into separate module
    cf. http://docs.python.org/2/library/__main__.html
    
    not sure this is entirely correct though

--- ttws/__init__.py
+++ ttws/__init__.py
@@ -21,7 +21,6 @@ it acts only on files with a given file extension listed in 'extstring'.
 """
 from __future__ import absolute_import, with_statement
 
-import sys
 import os
 import textwrap
 
@@ -38,36 +37,6 @@ BLACKLIST = ['.bzr', '.cvs', '.git', '.hg', '.svn', '.#']
 
 # convert the string object to a list object
 listofexts  = extstring.split(",")
-
-def usage(args):
-    """Help message on usage."""
-    message = """
-        Usage: %s [OPTIONS] <directory> [<directory> ...]
-
-         This script will recursively remove all trailing white spaces in all
-         text files in a given list directories. Binary files and files residing in
-         '.bzr', '.cvs', '.git', '.hg', '.svn' directories are skipped.
-
-        Note for Windows users:
-         If you have not libmagic installed the script will fallback to only
-         trim files with the following extensions: %s
-
-        Options:
-            -h, --help
-                displays this help message
-
-            -s, --strip
-                strips leading or trailing white spaces from info or
-                revision strings that contain HTML documentation
-                (those disturb the proper HTML rendering in 'some' tools)
-
-            -c, --clean
-                WARNING: USE THIS OPTION AT YOUR OWN RISK AS IT *WILL* BREAK YOUR CODE!
-                Removes obsolete or superfluous annotation constructs
-                from Modelica files. Only use this if your code is under version control
-                and in combination with a careful code-diff review.
-        """ % (os.path.split(args[0])[1],extstring,)
-    print textwrap.dedent(message)
 
 def unkownOption(args):
     """Warning message for unknown options."""
@@ -106,63 +75,6 @@ def detecttype(filepath):
             return "text"
         else:
             return "unknown"
-
-def main(args):
-    import getopt
-    # Look for optional arguments:
-    try:
-        opts, dirnames = getopt.getopt(args[1:], "hsc", ["help","strip","clean"])
-    # Unknown option is given trigger the display message:
-    except getopt.GetoptError:
-        unkownOption(args)
-        sys.exit(0)
-    # if no dir name is given print the usage message
-    if not dirnames:
-        usage(args)
-        sys.exit(0)
-
-    # If help option is given display help otherwise display warning:
-    cleanOpt = False
-    stripOpt = False
-    for opt, arg in opts:
-        if opt in ("-h","--help"):
-            usage(args)
-            sys.exit(0)
-        elif opt in ("-c","--clean"):
-            cleanOpt = True
-        elif opt in ("-s","--strip"):
-            stripOpt = True
-        else:
-            unkownOption(args)
-            sys.exit(0)
-
-    # Walk through the given path and call trim function for text files only:
-    for dirname in dirnames:
-        if os.path.exists(dirname) is False:
-            # Warn about an unknown directory
-            unkownDirectory(dirname)
-        else:
-            for path, dirs, files in os.walk(dirname):
-                for directory in dirs:
-                    if directory in BLACKLIST:
-                        print "skipping version control dir: %s " % directory
-                        dirs.remove(directory)
-                for file in files:
-                    filepath = os.path.join(path, file)
-                    filetype = detecttype(filepath)
-                    if filetype is "mo" and cleanOpt is True:
-                        print "trimming and cleaning %s" % filepath
-                        trimWhitespace(filepath)
-                        cleanAnnotation(filepath)
-                    elif filetype is "mo" and stripOpt is True:
-                        print "trimming and stripping %s" % filepath
-                        trimWhitespace(filepath)
-                        stripDocString(filepath)
-                    elif filetype is "mo" or filetype is "text":
-                        print "trimming %s" % filepath
-                        trimWhitespace(filepath)
-                    else:
-                        print "skipping file of type %s: %s" % (filetype, filepath)
 
 def trimWhitespace(filepath):
     """Trim trailing white spaces from a given filepath."""
@@ -243,6 +155,3 @@ def stripDocString(filepath):
 
     with open(filepath,'w') as mo_file:
         mo_file.write(out)
-
-if __name__ == "__main__":
-    sys.exit(main(sys.argv))
---
+++ ttws/__main__.py
@@ -0,0 +1,97 @@
+import sys
+import os
+import getopt
+
+from . import extstring, textwrap
+
+
+def main(args):
+    # Look for optional arguments:
+    try:
+        opts, dirnames = getopt.getopt(args[1:], "hsc", ["help","strip","clean"])
+    # Unknown option is given trigger the display message:
+    except getopt.GetoptError:
+        unkownOption(args)
+        sys.exit(0)
+    # if no dir name is given print the usage message
+    if not dirnames:
+        usage(args)
+        sys.exit(0)
+
+    # If help option is given display help otherwise display warning:
+    cleanOpt = False
+    stripOpt = False
+    for opt, arg in opts:
+        if opt in ("-h","--help"):
+            usage(args)
+            sys.exit(0)
+        elif opt in ("-c","--clean"):
+            cleanOpt = True
+        elif opt in ("-s","--strip"):
+            stripOpt = True
+        else:
+            unkownOption(args)
+            sys.exit(0)
+
+    # Walk through the given path and call trim function for text files only:
+    for dirname in dirnames:
+        if os.path.exists(dirname) is False:
+            # Warn about an unknown directory
+            unkownDirectory(dirname)
+        else:
+            for path, dirs, files in os.walk(dirname):
+                for directory in dirs:
+                    if directory in BLACKLIST:
+                        print "skipping version control dir: %s " % directory
+                        dirs.remove(directory)
+                for file in files:
+                    filepath = os.path.join(path, file)
+                    filetype = detecttype(filepath)
+                    if filetype is "mo" and cleanOpt is True:
+                        print "trimming and cleaning %s" % filepath
+                        trimWhitespace(filepath)
+                        cleanAnnotation(filepath)
+                    elif filetype is "mo" and stripOpt is True:
+                        print "trimming and stripping %s" % filepath
+                        trimWhitespace(filepath)
+                        stripDocString(filepath)
+                    elif filetype is "mo" or filetype is "text":
+                        print "trimming %s" % filepath
+                        trimWhitespace(filepath)
+                    else:
+                        print "skipping file of type %s: %s" % (filetype, filepath)
+
+
+def usage(args):
+    """Help message on usage."""
+    message = """
+        Usage: %s [OPTIONS] <directory> [<directory> ...]
+
+         This script will recursively remove all trailing white spaces in all
+         text files in a given list directories. Binary files and files residing in
+         '.bzr', '.cvs', '.git', '.hg', '.svn' directories are skipped.
+
+        Note for Windows users:
+         If you have not libmagic installed the script will fallback to only
+         trim files with the following extensions: %s
+
+        Options:
+            -h, --help
+                displays this help message
+
+            -s, --strip
+                strips leading or trailing white spaces from info or
+                revision strings that contain HTML documentation
+                (those disturb the proper HTML rendering in 'some' tools)
+
+            -c, --clean
+                WARNING: USE THIS OPTION AT YOUR OWN RISK AS IT *WILL* BREAK YOUR CODE!
+                Removes obsolete or superfluous annotation constructs
+                from Modelica files. Only use this if your code is under version control
+                and in combination with a careful code-diff review.
+        """ % (os.path.split(args[0])[1],extstring,)
+    print textwrap.dedent(message)
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
